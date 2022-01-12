db633545bf38038f0a0f0bb3f9af6664baf9dc72
JoonasPitk <46161@edu.raseko.fi>
2022-01-12 08:57:40 +0200

    Create testi.py

---
+++ testi.py
@@ -0,0 +1,12 @@
+# Ensimmäinen harjoitus. Muuttujat, näppäimistö ja näyttö.
+
+# Määritellään muuttujat ja kysytään arvot käyttäjältä.
+
+first_name = input('Mikä on etunimesi? ')
+favourite_place = input('Mikä on suosikkipaikkasi? ')
+nickname = input('Mikä on lempinimesi? ')
+year_of_birth = input('Mikä on syntymävuotesi? ')
+
+# Tulostetaan tiedot ruudulle.
+
+print(first_name, 'olet syntynyt', year_of_birth)
